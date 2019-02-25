f94701980096672330b1cd4681f12bc1273187fd
Alexander Maunonen <santari33@gmail.com>
2019-02-25 14:57:00 +0200

    fibonacci series

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,29 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+// My solution of Fibonacci
+
+
+// linear runtime complexity 
+function fib (n){
+   const result = [0, 1] ; 
+   for (let i = 2 ; i <=n; i++){
+        const a = result[i -1 ]; 
+        const b = result [i - 2]; 
+        result.push(a+b); 
+   }
+   return result[n];
+}
+function fibMySolutions(n) {
+    debugger; 
+    let arrFib = [0, 1]; 
+    for (let i = 1 ; i <  n ; i++){
+
+        arrFib[i + 1] = arrFib[i] + arrFib[i-1]; 
+    }
+    return arrFib[arrFib.length-1]; 
+}
+fib(4);
 
 module.exports = fib;
--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,85 +15,116 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {
+
+// tutor Loop solution
+function matrix(n){
+
+    const results = []; 
+    for (let i = 0 ; i < n ; i++){
+        results.push([]); 
+    }
+
+    // declare vorder of matrix 
+    let counter = 1; 
+    let starttColumn = 0; 
+    let endColumn = n -1; 
+    let startRow = 0 ; 
+    let endRow = n - 1; 
+    debugger; 
+    while (starttColumn <= endColumn && startRow <= endRow){
+        for (let i = starttColumn; i <= endColumn; i++ ){
+            results[startRow][i] = counter; 
+            counter++; 
+        }
+        // increement start row 
+        debugger;  
+        startRow++;
+        
+        for (let i = startRow; i <=endRow; i++){
+            results[i][endColumn] = counter; 
+            counter++; 
+        }
+        endColumn--;
+         
+        for (let  i = endColumn ; i >= starttColumn; i-- ){
+            results[endRow][i] = counter; 
+            counter++; 
+        }
+        endRow--; 
+
+        for (let i = endRow; i >= startRow; i--){
+            results[i][starttColumn] = counter; 
+            counter++; 
+        }
+        starttColumn++; 
+
+
+    }
+    return results; 
+
+}
+
+function matrixMySolutoins(n) {
     let j = 0; 
     let i = 0;
     let count = 0; 
     let arr = []; 
+    // create the empty array of results 
     for (let ar = 0 ; ar < n ; ar++){
         arr[ar] = new Array(n); 
     }
-    debugger; 
+    //debugger; 
     let leftCornerI = 0;
     let leftCornerJ = 0; 
-    
-
-/*      for (let p = 0; p < 4 ; p++){
-        
-        for (let d = 0; d < 4 ; d++ ){
-             arr[p, d] = undefined;  
-             console.log(arr[p,d]);
-        } 
-    }  */
 
     do{
         // Gow right till end of row 
-        
-
         while (i < n ){
-            
             count++; 
             arr[j][i] = count; 
             console.log('go right  arr ',+j+','+ i+ '   '+    arr[j][i] ); 
             i++;
             
         }
-        
+        // decremetn i and encrement J 
         i--;
         j++;
         leftCornerJ ++;
 
+        //checking if the next cell is empty 
         if ( arr[j][i] ){
             break; 
         }
 
-        
-        while (j < n ){
-            
+        // loop bottom till n 
+        while (j < n ){            
             count++; 
             arr[j][i] = count; 
             console.log('go bottom  arr ',+j+','+ i +  '   ' +  arr[j][i] ); 
             j++;
         }
-         
+        
+        // decreement j 
         j--;
         
-
+        //checking if the next cell is not empty 
         if ( arr[j][i-1] ){
             break; 
         }
 
-        //count--;
-        
-        
-        // go Left till 0 
-        //console.log(i, leftCornerI); 
-        
-        
+        // go Left till left corner i
         while (i >  leftCornerI){
             count++; 
             i--; 
             arr[j][i] = count; 
             console.log('go left  arr ',+j+','+ i+  '   ' +  arr[j][i] ); 
-            
         }  
         j--;
-
+        // checking if the naxt cell is not empty
         if ( arr[j][i] ){
             break; 
         }
 
-        
         // go up till arr !== null
         while (j >= leftCornerJ ){
             count++; 
@@ -103,20 +134,19 @@ function matrix(n) {
         } 
         leftCornerI++; 
         j++; 
-        
-
+        // go up till arr !== null
         if ( arr[j][i +1 ] ){
             break; 
         }
 
-
-        debugger; 
+        //debugger; 
         n --; 
         i++;
-        //console.log(arr[j, i]); 
+    
     } while (n !== 0)
     return arr;
 }
 
 matrix (4); 
 module.exports = matrix;
+
