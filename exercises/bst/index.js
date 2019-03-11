e97e6841a405818ec626dbe1b9267055e833a3e2
Alexander Maunonen <santari33@gmail.com>
2019-03-11 11:14:25 +0200

    validate b-tree, Binary search tree

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,66 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data){
+        this.data = data; 
+        this.left = null; 
+        this.right = null; 
+    }
+    insert (data){
+        // go right 
+        if (data < this.data && this.left ){
+            this.left.insert(data);   
+        } else if (data < this.data ){
+            this.left = new Node(data); 
+        } else if (data > this.data && this.right ){
+            this.right.insert(data); 
+        } else if  (data > this.data){
+            this.right =  new Node (data); 
+        }
+    }
+
+    contains(data){
+        if (this.data === data){
+            return this ; 
+        }
+        if (this.data  < data && this.right ){
+            return this.right.contains(data);  
+        } else if (this.data > data && this.left ){
+            return this.left.contains(data); 
+        }
+        return null; 
+    }
+
+    // own solution doesn't work something goes wrong 
+    containsOwn (data){
+        debugger; 
+        if (data === this.data){
+            return this; 
+        } 
+        if (data > this.data && this.right ){
+            return this.right.contains(data); 
+        } else if (data > this.data) {
+            return null; 
+        } else if (data < this.data && this.left ){
+            return this.left.contains(data); 
+        }
+        else if (data < this.data){
+            return null; 
+        }
+    }
+}
+
+const node = new Node(10);
+node.insert(5);
+node.insert(15);
+node.insert(20);
+node.insert(0);
+node.insert(-5);
+node.insert(3);
+node.contains(-5); 
+
+
+
 
 module.exports = Node;
